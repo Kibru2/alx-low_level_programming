cae548e7a102b7a2f4976800a4aa540840128c58
kibru <kibru2@github.com>
2023-02-16 08:04:36 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that prints with put function
+ * Return: 0 (Done)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
