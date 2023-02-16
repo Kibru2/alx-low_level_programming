f32263aaa118f3ce72e4446282ef9e3214b032ce
kibru <kibru2@github.com>
2023-02-16 11:37:04 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints with printf
+ * return: 0 (done)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")		return (0);
+}
