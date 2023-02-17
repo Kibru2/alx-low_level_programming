11cfbae590e61055c8b8b103ac48bf47e36b7941
kibru <kibru2@github.com>
2023-02-17 06:38:30 -0800

    4-print_alphabt

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,22 +7,22 @@
 
 int main(void)
 {
-        char c;
-
-        char d;
-
-        c = 'a';
-        d = 'A';
-        while
-                (c <= 'z') {
+	char c;
+	
+	char d;
+	
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
 			putchar(c);
-                        c++;
-                }
-        while
-                (d <= 'Z') {
-                        putchar(d);
-                        d++;
-                }
-        putchar('\n');
-        return (0);
+			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(d);
+			d++
+		}
+	putchar('\n');
+	return (0);
 	}
