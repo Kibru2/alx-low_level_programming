1b3e5c810071a35eebe6ff20398fa6716fdd010e
kibru <kibru2@github.com>
2023-02-17 07:50:10 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program that prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+	
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
