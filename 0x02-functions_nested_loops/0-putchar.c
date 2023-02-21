ab72447a4e9bc8176211f745f9228a11a853380a
kibru <kibru2@github.com>
2023-02-21 11:39:23 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,9 +10,9 @@ int main(void)
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-        _putchar('t');
+	_putchar('t');
 	_putchar('c');
-        _putchar('h');
+	_putchar('h');
 	_putchar('r');
 	_putchar('\n');
 	return (0);
