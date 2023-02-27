c308f8ffabc81ad0ab76a5a2d0f0a1dedc4b062b
kibru <kibru2@github.com>
2023-02-27 04:15:01 -0800

    Half the lies they tell about me aren't true

--- 0x04-more_functions_nested_loops/6-puts2.c
+++ 0x04-more_functions_nested_loops/6-puts2.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character starting with the first
- * character, followed by a new line.
- * @str: the string to manipulate.
- *
- * Return: nothing.
+ * puts2 - print out even parts of string.
+ * @str: string to print.
+ * Return: void.
  */
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-		if (!(i % 2))
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
 			_putchar(str[i]);
-	_putchar(10);
+	}
+	_putchar('\n');
 }
