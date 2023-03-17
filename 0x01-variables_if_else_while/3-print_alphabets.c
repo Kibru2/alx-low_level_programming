c12a7769a51fba218ffbb0005fdc179c92f155c3
kibru <kibru2@github.com>
2023-03-17 05:37:06 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
-/**
- * main - print alphabets in lower case, and then in uppercase
- *
- * Return: Always 0 (success)
- */
+/* more headers goes there */
 
+/**
+* main - print lowercases alphabet
+* followed by uppercase alphabets
+* Return: program returns 0
+*/
 int main(void)
 {
-	char c;
-	
-	char d;
-	
-	c = 'a';
-	d = 'A';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-	while
-		(d <= 'Z') {
-			putchar(d);
-			d++
-		}
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
 	putchar('\n');
 	return (0);
 }
