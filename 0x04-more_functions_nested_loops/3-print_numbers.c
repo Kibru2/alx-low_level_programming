091791055f2220102b64b8929265d947e91c68a0
kibru <kibru2@github.com>
2023-03-23 05:58:55 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,13 @@
-
+#include "main.h"
 /**
- * print_numbers - Print the numbers since 0 up to 9
+ * print_numbers - Prints the numbers from 0 to 9
  *
- * Return: The numbers since 0 up to 9
+ * Return: void
  */
 void print_numbers(void)
 {
-	int i;
-
-	for (i = 0, i <=9, i++)
-		_putchar(i + '0')
-	_putchar('\n')
+	int num;
+		for (num = 0; num < 10; num++)
+		_putchar(num + '0');
+		_putchar('\n');
 }
-
-
