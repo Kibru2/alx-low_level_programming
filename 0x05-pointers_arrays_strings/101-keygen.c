7af88dfe1a13dcc51f9fb4df39c09d871f8c88ec
kibru2 <kibru2@github.com>
2023-05-30 01:17:25 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand();
+	printf("%i\n", n);
+	return (0);
+}
