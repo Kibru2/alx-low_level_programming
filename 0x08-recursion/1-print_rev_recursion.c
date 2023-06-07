8d70512e59fc885c7bc1c130ae5e3dde401486e0
kibru2 <kibru2@github.com>
2023-06-07 06:49:50 -0700

    committed

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - a function that prints a string in reverse,
+ * @s: an input string to print
+ * Return: Nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
